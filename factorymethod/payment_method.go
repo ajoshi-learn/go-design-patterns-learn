a4bb6045d5c12ef3c3b6eccd48dde92b09b2c789
ajoshi <a.joshi@levi9.com>
2018-08-01 11:59:49 +0300

    factory method

---
+++ factorymethod/payment_method.go
@@ -0,0 +1,39 @@
+package factorymethod
+
+import (
+	"errors"
+	"fmt"
+)
+
+type PaymentType int
+
+const (
+	Cash      PaymentType = iota
+	DebitCard
+)
+
+type PaymentMethod interface {
+	Pay(amount float32) string
+}
+
+func GetPaymentMethod(paymentType PaymentType) (PaymentMethod, error) {
+	switch paymentType {
+	case Cash:
+		return new(CashPM), nil
+	case DebitCard:
+		return new(DebitPM), nil
+	default:
+		return nil, errors.New(fmt.Sprintf("Payment method %d is not recognized", paymentType))
+	}
+}
+
+type CashPM struct{}
+type DebitPM struct{}
+
+func (c *CashPM) Pay(amount float32) string {
+	return fmt.Sprintf("%0.2f paid using cash\n", amount)
+}
+
+func (c *DebitPM) Pay(amount float32) string {
+	return fmt.Sprintf("%0.2f paid using debit card\n", amount)
+}
---
+++ factorymethod/payment_method_test.go
@@ -0,0 +1,40 @@
+package factorymethod
+
+import (
+	"testing"
+	"strings"
+)
+
+func TestCreatePaymentMethodCash(t *testing.T) {
+	paymentMethod, err := GetPaymentMethod(Cash)
+	if err != nil {
+		t.Fatal("A payment method 'Cash' shoud exist")
+	}
+
+	msg := paymentMethod.Pay(10.30)
+	if !strings.Contains(msg, "paid using cash") {
+		t.Error("The cash payment method message wasn't correct")
+	}
+	t.Log("LOG:", msg)
+}
+
+func TestCreatePaymentMethodDebit(t *testing.T) {
+	paymentMethod, err := GetPaymentMethod(DebitCard)
+	if err != nil {
+		t.Fatal("A payment method 'DebitCard' shoud exist")
+	}
+
+	msg := paymentMethod.Pay(10.30)
+	if !strings.Contains(msg, "paid using debit card") {
+		t.Error("The debit card payment method message wasn't correct")
+	}
+	t.Log("LOG:", msg)
+}
+
+func TestGetPaymentMethodNotExistent(t *testing.T) {
+	_, err := GetPaymentMethod(10)
+	if err == nil {
+		t.Error("A payment method 10 must return an error")
+	}
+	t.Log("LOG:", err)
+}
